f8dd57019d8983dd3923417873005108143711ec
1220505053 <127299631+1220505053@users.noreply.github.com>
2023-04-03 20:47:49 +0300

    Add files via upload

---
+++ DallarBirbirineEsMi.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace DallarBirbirineEsitMi
+{
+
+    class Node  //Dallari tanimliyoruz
+    {
+        public int sayi;
+        public Node sol, sag;
+
+        public Node(int sayi)
+        {
+            sayi = sayi;    //Ilk degelerini sifira esitliyoruz
+            sol = sag = null;
+        }
+    }
+
+    class Program
+    {
+        static bool Aynilarmi(Node a, Node b)   //Dallar ayni mi diye kontrol eden kisim
+        {
+            if (a == null && b == null)
+            {
+                return true;
+            }
+            else if (a == null || b == null)
+            {
+                return false;
+            }
+            else if (a.sayi != b.sayi)
+            {
+                return false;
+            }
+            else
+            {
+                return Aynilarmi(a.sol, b.sol) && Aynilarmi(a.sag, b.sag);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // İlk ikili ağaç oluşturuluyor
+            /* Dallardaki atamayi buradan yapiyoruz. 
+            Node() ifadesindeki parantez icine istediginiz sayilari girebilirsiniz */
+            Node AgacA = new Node(1);
+            AgacA.sol = new Node(2);
+            AgacA.sag = new Node(3);
+            AgacA.sol.sol = new Node(4);
+            AgacA.sol.sag = new Node(5);
+
+            // İkinci ikili ağaç oluşturuluyor
+            /* Dallardaki atamayi buradan yapiyoruz. 
+           Node() ifadesindeki parantez icine istediginiz sayilari girebilirsiniz */
+            Node AgacB = new Node(1);
+            AgacB.sol = new Node(2);
+            AgacB.sag = new Node(3);
+            AgacB.sol.sol = new Node(4);
+            AgacB.sol.sag = new Node(5);
+
+            // İki ikili ağacın aynı olup olmadığına gore cikti sagliyor
+            if (Aynilarmi(AgacA, AgacB))
+            {
+                Console.WriteLine("Agaclar aynidir.");
+            }
+            else
+            {
+                Console.WriteLine("Agaclar ayni degildir.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,16 @@
+C# dilinde girdi olarak verilen iki tane ikili ağacın A ve B şeklinde kökleri verildiğinde, bunların aynı olup olmadığını kontrol eden 
+bir fonksiyondur. BinaryTreelerdeki veriler ayni mi degil kontrol edilmekte kullanilabilir.
+
+Indirdigimiz dosyalari calistirip ustunde degisiklikler yapmak icin C# diline entegre bir uygulamaya ihtiyacimiz var. Benim onerim Visual Studio olacak.
+Visual Studio' yu ucretsiz bir sekilde Micrososft' un web sitesinden indirebilirsiniz.
+
+Visual Studio 2022 indirmek icin asagidaki linki kullanabilirsiniz:
+https://visualstudio.microsoft.com/downloads/
+
+ Visual Studio kurulumu bittikten sonra .cs dosyalarina su sirayla erisebilirisniz:
+ 1- Visual Studio' yu acin ve "File -> New -> Project" sirasini takip edin
+ 2- "Console App" basligini listeden secin ve isimlendirin.
+ 3- Proje olusturulduktan sonra "Program.cs" seklinde isimlendirildigini goreceksiniz. Bu kutu altinda yazilan kodlarin islevsel bir sekilde bir arada durmasini sagliyor.
+ 4- Sayfanin en ust kismindaki Visual Studio ikonunun yanindaki "File -> Open -> File -> "Yukledigimiz Dosya" -> Open" yaparak doyayi aciyoruz.
+ 5- F5 tusuna basarak direkt programi calistirabilirsinz
+ NOT: Kodlardaki yesil yazilari okuyarak kodlarin islevini ogrenebilir ve istediginiz gibi degistirebilirsiniz
